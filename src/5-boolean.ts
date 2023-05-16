3f4a13f3b5fe7b922eb9be85bcaefb41a227258a
gara84k <kikeortiz925@gmail.com>
2023-05-15 20:05:38 -0500

    se agrega los ejemplos

---
+++ src/5-boolean.ts
@@ -0,0 +1,14 @@
+(()=>{
+    let isEnable = true;
+    // isEnable = 'as';
+    // isEnable = 1212
+    isEnable = false;
+
+    let isNew: boolean = false;
+    console.log('isNew: ', isNew);
+    isNew = true;
+    console.log('isNew : ', isNew);
+
+    const random = Math.random();
+    console.log('random : ', random);
+})
